810a80eec25199b9862843d4edb916e68d011c23
Oscar Gonzalez <alexkraken.mx@gmail.com>
2021-06-01 17:26:10 -0500

    Carousel updated

--- svelte_project/src/UI/Carousel.svelte
+++ svelte_project/src/UI/Carousel.svelte
@@ -1,30 +1,64 @@
-<div class="container my-4">
+<div>
     <div
     id="carouselExampleControls"
     class="carousel slide"
     data-bs-ride="carousel"
     >
     <div class="carousel-inner">
-        <div class="carousel-item active">
+        <div class="carousel-item active" data-bs-interval="10000">
         <img
             src="https://www.brazaletesmexico.com/wp-content/uploads/brazaletes-en-mexico-de-seguridad-tyvek2.jpg"
-            class="d-block w-100"
-            alt="https://via.placeholder.com/600x250"
+            class="card-img" 
+            alt=""
         />
+            <div class="card-img-overlay container px-5 d-flex align-items-center">
+                <div class="col-5">
+                    <img src="https://www.brazaletesmexico.com/wp-content/uploads/brazaletes-mexico-tyvek.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-6 offset-1">
+                    <h1 class="display-6 fw-bold">Brazaletes <br> Full Color</h1>
+                    <h5 class="fw-bold">Promociona tu evento mientras lo controlas...</h5>
+                    <button class="btn btn-secondary shadow  mt-3">Ver más</button>
+                </div>
+            </div>
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item" data-bs-interval="10000">
         <img
             src="https://www.brazaletesmexico.com/wp-content/uploads/banner-tienda-en-linea.jpg"
-            class="d-block w-100"
-            alt="https://via.placeholder.com/600x250"
+            class="card-img"
+            alt=""
         />
+        <div class="card-img-overlay container px-5 d-flex align-items-center">
+            <div class="col-6">
+                <h1 class="text-start display-6 fw-bold">Diseños preestablecidos</h1>
+                <h5 class="fw-bold">Rápidos tiempos de entrega</h5>
+                <button class="btn btn-secondary shadow  mt-3">Comprar</button>
+            </div>
+            <div class="col-4">
+                <img src="https://www.brazaletesmexico.com/wp-content/uploads/OBJETO-TIENDA-ONLINE.png" class="img-fluid" alt="">
+            </div>
         </div>
-        <div class="carousel-item">
+        </div>
+        <div class="carousel-item" data-bs-interval="10000">
         <img
             src="https://www.brazaletesmexico.com/wp-content/uploads/brazaletes-rfdi-en-mexico-seguridad.jpg"
-            class="d-block w-100"
-            alt="https://via.placeholder.com/600x250"
+            class="card-img"
+            alt=""
         />
+        <div class="card-img-overlay container px-5 d-flex align-items-center">
+            <div class="p-3 col-8 text-white" style="background-color: rgba(0, 0, 0, 0.5);">
+                <h1 class="text-start display-6 text-white fw-bold">Brazaletes RFID</h1>
+                <h4 class="text-white fw-bold">Tecnología en Radiofrecuencia como:</h4>
+                <ul>
+                    <li>Abrir cerraduras</li>
+                    <li>Centro de consumo</li>
+                    <li>Identificación de cliente</li>
+                </ul>
+                <div class="text-end">
+                    <button class="btn btn-secondary shadow">Ver más</button>
+                </div>
+            </div>
+        </div>
         </div>
     </div>
     <button
