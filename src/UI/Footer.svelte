43a17fed61fd3b9e3104f21ff45f427461f0ca50
Julian Candiani <jcandianivelez@gmail.com>
2021-06-02 10:43:36 -0500

    feat(home): Creación del home del sitio con los recursos de brazaletes.
    - Header
    - Slider
    - Grid de cotización
    - Grid de eventos
    - Grid de ventajas
    - Footer
    
    Changes to be committed:
    	modified:   .gitignore
    	renamed:    svelte_project/README.md -> README.md
    	renamed:    index.html -> _index.html
    	new file:   package-lock.json
    	renamed:    svelte_project/package.json -> package.json
    	renamed:    css/all.css -> public/css/all.css
    	renamed:    css/lux-bootstrap.css -> public/css/lux-bootstrap.css
    	renamed:    css/lux-bootstrap.min.css -> public/css/lux-bootstrap.min.css
    	renamed:    css/material-bootstrap.build.min.css -> public/css/material-bootstrap.build.min.css
    	renamed:    svelte_project/public/favicon.png -> public/favicon.png
    	renamed:    svelte_project/public/global.css -> public/global.css
    	renamed:    svelte_project/public/index.html -> public/index.html
    	renamed:    js/all.js -> public/js/all.js
    	renamed:    js/main.js -> public/js/main.js
    	renamed:    svelte_project/rollup.config.js -> rollup.config.js
    	renamed:    svelte_project/scripts/setupTypeScript.js -> scripts/setupTypeScript.js
    	renamed:    svelte_project/src/App.svelte -> src/App.svelte
    	renamed:    svelte_project/src/Pages/Home.svelte -> src/Pages/Home.svelte
    	renamed:    svelte_project/src/UI/BenefitBlockquote.svelte -> src/UI/BenefitBlockquote.svelte
    	renamed:    svelte_project/src/UI/BenefitsGrid.svelte -> src/UI/BenefitsGrid.svelte
    	renamed:    svelte_project/src/UI/Carousel.svelte -> src/UI/Carousel.svelte
    	renamed:    svelte_project/src/UI/EventCard.svelte -> src/UI/EventCard.svelte
    	renamed:    svelte_project/src/UI/EventsGrid.svelte -> src/UI/EventsGrid.svelte
    	renamed:    svelte_project/src/UI/Footer.svelte -> src/UI/Footer.svelte
    	new file:   src/UI/Header.svelte
    	new file:   src/UI/Menu.svelte
    	renamed:    svelte_project/src/UI/QuotationCard.svelte -> src/UI/QuotationCard.svelte
    	renamed:    svelte_project/src/UI/QuotationGrid.svelte -> src/UI/QuotationGrid.svelte
    	renamed:    svelte_project/src/UI/Section.svelte -> src/UI/Section.svelte
    	renamed:    svelte_project/src/UI/Template.svelte -> src/UI/Template.svelte
    	renamed:    svelte_project/src/main.js -> src/main.js
    	deleted:    svelte_project/package-lock.json
    	deleted:    svelte_project/public/build/bundle.css
    	deleted:    svelte_project/public/build/bundle.js
    	deleted:    svelte_project/public/build/bundle.js.map
    	deleted:    svelte_project/public/css/all.css
    	deleted:    svelte_project/public/css/lux-bootstrap.css
    	deleted:    svelte_project/public/css/lux-bootstrap.min.css
    	deleted:    svelte_project/public/css/material-bootstrap.build.min.css
    	deleted:    svelte_project/public/js/all.js
    	deleted:    svelte_project/public/js/main.js
    	deleted:    svelte_project/src/UI/Header.svelte

--- src/UI/Footer.svelte
+++ src/UI/Footer.svelte
@@ -2,39 +2,36 @@
     <div class="container">
     <div class="row">
         <div class="col">
-        <p><strong>BRAZALETES MÉXICO</strong></p>
-        <address class="small">
-            Sm 301, Mza 4, Lote 5, Central de <br>
-            Abastos, 77536, Cancún, Q.R. <br>
-            <abbr title="Phone">P:</abbr> (123) 456-7890
-        </address>
-        <address class="small">
-            <strong>Email contact</strong><br>
-            <a href="mailto:first.last@example.com">first.last@example.com</a>
-        </address>
-        <address class="small">
-            <strong>Phone Number 1</strong><br>
-            <a href="tel:555555555">555555555</a>
-        </address>
+            <p><strong>BRAZALETES MÉXICO</strong></p>
+            <address class="small">
+                Sm 301, Mza 4, Lote 5, Central de <br>
+                Abastos, 77536, Cancún, Q.R. <br>
+                <abbr title="Phone">P:</abbr> (123) 456-7890
+            </address>
+
+            <address class="small">
+                <strong>Phone Number 1</strong><br>
+                <a href="tel:555555555">555555555</a>
+            </address>
         </div>
 
         <div class="col">
         <p><strong>Referred Links</strong></p>
         <ul class="list-unstyled">
             <li class="mb-2 small">
-            <a class="link-light" href>About Us</a>
+                <a class="link-light" href>About Us</a>
             </li>
             <li class="mb-2 small">
-            <a class="link-light" href>Contact</a>
+                <a class="link-light" href>Contact</a>
             </li>
             <li class="mb-2 small">
-            <a class="link-light" href>Terms and conditions</a>
+                <a class="link-light" href>Terms and conditions</a>
             </li>
             <li class="mb-2 small">
-            <a class="link-light" href>Privacy policy</a>
+                <a class="link-light" href>Privacy policy</a>
             </li>
             <li class="mb-2 small">
-            <a class="link-light" href>Site Map</a>
+                <a class="link-light" href>Site Map</a>
             </li>
             <li class="small">
             <a class="link-light" href>Shipping</a>
