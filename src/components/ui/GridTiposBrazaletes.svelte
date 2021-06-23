b7ab196b689ceeb39b2f353291c011680cffc117
Oscar Alejandro <alexkraken.mx@gmail.com>
2021-06-23 01:16:29 -0500

    cambio del grid a mínimo dos cards

--- src/components/ui/GridTiposBrazaletes.svelte
+++ src/components/ui/GridTiposBrazaletes.svelte
@@ -3,7 +3,7 @@
     export let tipos = [];
 </script>
 
-<div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-4 justify-content-{tipos.length > 4 ? 'start' : 'center'} g-3">
+<div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 justify-content-{tipos.length > 4 ? 'start' : 'center'} g-3">
     {#each tipos as tipo (tipo.id)}
         <CardTipoBrazalete on:updateIdTipo {tipo} />
     {/each}
