88f868ec3f3381e4fcd08294c5c9d0cd63655348
Julian Candiani <jcandianivelez@gmail.com>
2021-06-23 22:46:12 -0500

    Se agrego la pagina de error...

---
+++ src/components/ui/FormCotizacion.svelte
@@ -0,0 +1,6 @@
+<form>
+    <div class="col-md-6">
+        <label for="" class="form-label"> Nombre * </label>
+        <input type="text" name="nombre" id="nombre" class="form-control form-control-sm">
+    </div>
+</form>
