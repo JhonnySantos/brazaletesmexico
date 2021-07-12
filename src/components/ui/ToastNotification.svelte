0b7c176a720af8abdc6f63e5289098ee358db159
Oscar Gonzalez <alexkraken.mx@gmail.com>
2021-07-12 12:22:56 -0500

    actualización del toast de notificaciones

--- src/components/ui/ToastNotification.svelte
+++ src/components/ui/ToastNotification.svelte
@@ -1,46 +1,38 @@
 <script>
   import { onMount } from "svelte";
+  export let notificacion = {};
 
   onMount(() => {
-    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
-    var toastList = toastElList.map(function (toastEl) {
-      return new bootstrap.Toast(toastEl, {});
-    });
+    const toastElement = document.querySelector(".toast");
+    const bsToast = new bootstrap.Toast(toastElement, {});
 
-    toastList.forEach((popup) => {
-      popup.show();
-    });
+    toastElement.addEventListener("hidden.bs.toast", () =>
+      toastElement.parentElement.parentElement.remove()
+    );
+    bsToast.show();
   });
 </script>
 
 <div class="fixed-top">
-
-<div class="toast-container position-absolute end-0 mt-10 p-3">
-
-  <div
-    class="toast border-primary border-2 d-flex align-items-center p-3"
-    role="alert"
-    aria-live="assertive"
-    aria-atomic="true"
-  >
-    <p class="text-center mx-2 mb-0">
-      <i class="fas fa-info-circle fa-4x"></i>
-    </p>
-    <div class="toast-body mb-0 fw-bold">
-      <p>Hello, world! This is a toast message.</p>
+  <div class="toast-container position-absolute end-0 p-3 mt-9">
+    <div
+      class="toast border-{notificacion.colorFondo} border-2 d-flex align-items-center p-3"
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+    >
+      <p class="text-center text-{notificacion.colorTexto} mx-2 mb-0">
+        <i class="fas {notificacion.icono} fa-4x" />
+      </p>
+      <div class="toast-body mb-0 fw-bold text-{notificacion.colorTexto}">
+        <p>{notificacion.mensaje}</p>
+      </div>
     </div>
   </div>
-
-</div>
-
 </div>
 
 <style>
-  .top-20 {
-    top: 20% !important;
-  }
-
-  .mt-10 {
-    margin-top: 10rem !important;
+  .mt-9 {
+    margin-top: 9rem !important;
   }
 </style>
