3e53959ce303e0dff5ddccbae3b28fa8bcb37d35
Julian Candiani <jcandianivelez@gmail.com>
2021-07-13 00:32:35 -0500

    Notificaciones Toast

--- src/components/ui/ToastNotification.svelte
+++ src/components/ui/ToastNotification.svelte
@@ -1,17 +1,5 @@
 <script>
   import { onMount } from "svelte";
-<<<<<<< HEAD
-
-  onMount(() => {
-    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
-    var toastList = toastElList.map(function (toastEl) {
-      return new bootstrap.Toast(toastEl, {});
-    });
-
-    toastList.forEach((popup) => {
-      popup.show();
-    });
-=======
   export let notificacion = {};
 
   onMount(() => {
@@ -22,41 +10,10 @@
       toastElement.parentElement.parentElement.remove()
     );
     bsToast.show();
->>>>>>> ba9bbf3ffde394e81d16a6973057dd3ad0b44aae
   });
 </script>
 
 <div class="fixed-top">
-<<<<<<< HEAD
-
-<div class="toast-container position-absolute end-0 mt-10 p-3">
-
-  <div
-    class="toast border-primary border-2 d-flex align-items-center p-3"
-    role="alert"
-    aria-live="assertive"
-    aria-atomic="true"
-  >
-    <p class="text-center mx-2 mb-0">
-      <i class="fas fa-info-circle fa-4x"></i>
-    </p>
-    <div class="toast-body mb-0 fw-bold">
-      <p>Hello, world! This is a toast message.</p>
-    </div>
-  </div>
-
-</div>
-
-</div>
-
-<style>
-  .top-20 {
-    top: 20% !important;
-  }
-
-  .mt-10 {
-    margin-top: 10rem !important;
-=======
   <div class="toast-container position-absolute end-0 p-3 mt-9">
     <div
       class="toast border-{notificacion.colorFondo} border-2 d-flex align-items-center p-3"
@@ -77,6 +34,5 @@
 <style>
   .mt-9 {
     margin-top: 9rem !important;
->>>>>>> ba9bbf3ffde394e81d16a6973057dd3ad0b44aae
   }
 </style>
