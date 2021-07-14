672bbcab00e1032823b40d127afd19ee40976e95
Julian Candiani <jcandianivelez@gmail.com>
2021-07-14 09:06:46 -0500

    Se modificó la ruta del api

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -3,4 +3,4 @@ import { writable } from 'svelte/store'
 export const currentSection = writable(-1)
 export const currentIdTipo = writable(localStorage.getItem('currentIdTipo') || -1)
 // export const apiHost = writable('https://brazaletes-mexico-api.herokuapp.com/api/v1');
-export const apiHost = writable('http://localhost/api/v1')
+export const apiHost = writable('http://142.4.3.24/api/v1')
