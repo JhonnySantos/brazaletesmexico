5768215d516d78f8ebbcf8e0df602415e86ff6e3
Oscar Alejandro <alexkraken.mx@gmail.com>
2021-07-19 22:40:50 -0500

    limitación de caractéres válidos

--- src/components/ui/Buscador.svelte
+++ src/components/ui/Buscador.svelte
@@ -1,5 +1,6 @@
 <script>
   import { navigate } from "svelte-routing";
+import { prevent_default } from "svelte/internal";
   import { apiHost } from "../../stores/stores";
   import ListBusqueda from "./ListBusqueda.svelte";
 
@@ -7,15 +8,24 @@
   let showListResults = false;
   export let resultadosBusqueda = [];
 
-  const handleKeyupSearch = async () => {
-    if (inputSearch.length > 3) {
-      resultadosBusqueda = await obtenerBusqueda();
-      showListResults = true;
-    } else {
-      showListResults = false;
-      resultadosBusqueda = [];
-    }
+  const handleKeyupSearch = async ( event ) => {
+    
+      if (inputSearch.length > 3) {
+        if ( (event.keyCode >= 65 && event.keyCode <= 90) || event.keyCode == 192 ) {
+          resultadosBusqueda = await obtenerBusqueda();
+          showListResults = true;
+        }
+      } else {
+        showListResults = false;
+        resultadosBusqueda = [];
+      }
   };
+
+  const handleKeypressSearch = ( event ) => {
+    if ( !( event.keyCode == 192 || (event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 37 && event.keyCode <= 40) || event.keyCode == 8 || event.keyCode == 13 || event.keyCode == 16 || event.keyCode == 46 ) ) {
+      event.preventDefault();
+    }
+  }
 
   const obtenerBusqueda = async () => {
     let response = await fetch(`${$apiHost}/search/${inputSearch}`);
@@ -45,6 +55,7 @@
       aria-describedby="button-addon-search"
       bind:value={inputSearch}
       on:keyup={ handleKeyupSearch }
+      on:keydown={ handleKeypressSearch }
     />
     <button class="btn btn-secondary" type="submit" id="button-addon-search" on:click={ inputSearch.length > 3 && navigateToResultadosBusqueda }>
       Buscar
