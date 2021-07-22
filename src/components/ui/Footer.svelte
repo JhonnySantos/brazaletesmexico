3b4d1b429579a758a3ed5c8d9c4f80522271d23a
Oscar Alejandro <alexkraken.mx@gmail.com>
2021-07-21 20:35:00 -0500

    actualización y responsividad al <Footer>

--- src/components/ui/Footer.svelte
+++ src/components/ui/Footer.svelte
@@ -3,7 +3,7 @@
     const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="border-top pt-3">
+<footer class="border-top border-2 pt-3">
 
     <div class="container">
         <div class="row px-3">
@@ -17,19 +17,19 @@
                     <div class="col">
                         <small><strong class="d-block fw-bold">Cancún</strong></small>
                         <span>
-                            Oficina: <a class="link-info text-decoration-none" href="tel:9982068980">(998) 206-8980</a>
+                            Oficina: <a class="link-dark text-decoration-none" href="tel:9982068980">(998) 206-8980</a>
                         </span>
                         <span class="d-block">
-                            WhatsApp: <a class="link-info text-decoration-none" target="_blank" href="https://wa.me/529982404801?text=Estoy%20interesado%20en%20los%20brazaletes">(998) 240-4801</a>
+                            WhatsApp: <a class="link-dark text-decoration-none" target="_blank" href="https://wa.me/529982404801?text=Estoy%20interesado%20en%20los%20brazaletes">(998) 240-4801</a>
                         </span>
                     </div>
                     <div class="col">
                         <small><strong class="d-block fw-bold">CDMX</strong></small>
                         <span class="d-block">
-                            Oficina: <a class="link-info text-decoration-none" href="tel:5552929108">(55) 529-29108</a>
+                            Oficina: <a class="link-dark text-decoration-none" href="tel:5552929108">(55) 529-29108</a>
                         </span>
                         <span class="d-block">
-                            WhatsApp:  <a class="link-info text-decoration-none" target="_blank" href="https://wa.me/525518081015?text=Estoy%20interesado%20en%20los%20brazaletes">(551) 808-1015</a>
+                            WhatsApp:  <a class="link-dark text-decoration-none" target="_blank" href="https://wa.me/525518081015?text=Estoy%20interesado%20en%20los%20brazaletes">(551) 808-1015</a>
                         </span>
                     </div>
                 </div>
@@ -39,22 +39,22 @@
                 <p><strong class="fw-bold">SÍGUENOS</strong></p>
                 <ul class="list-unstyled">
                     <li class="mb-2 small">
-                        <a class="link-info text-decoration-none" target="_blank" href="https://www.facebook.com/brazaletesmex/">
+                        <a class="link-dark text-decoration-none" target="_blank" href="https://www.facebook.com/brazaletesmex/">
                             <i class="fab fa-facebook-square fa-lg"></i> @brazaletesmex
                         </a>
                     </li>
                     <li class="mb-2 small">
-                        <a class="link-info text-decoration-none" target="_blank" href="https://www.instagram.com/brazaletesmexico/">
+                        <a class="link-dark text-decoration-none" target="_blank" href="https://www.instagram.com/brazaletesmexico/">
                             <i class="fab fa-instagram-square fa-lg"></i> @brazaletesmexico
                         </a>
                     </li>
                     <li class="mb-2 small">
-                        <a class="link-info text-decoration-none" target="_blank" href="https://www.youtube.com/channel/UCljRrLXh0Ajd9ib0WYcb29A">
+                        <a class="link-dark text-decoration-none" target="_blank" href="https://www.youtube.com/channel/UCljRrLXh0Ajd9ib0WYcb29A">
                             <i class="fab fa-youtube-square fa-lg"></i> Brazaletes México SA DE CV
                         </a>
                     </li>
                     <li class="mb-4 small">
-                        <a class="link-info text-decoration-none" target="_blank" href="https://mx.linkedin.com/company/brazaletes-mexico">
+                        <a class="link-dark text-decoration-none" target="_blank" href="https://mx.linkedin.com/company/brazaletes-mexico">
                             <i class="fab fa-linkedin fa-lg"></i> Brazaletes México
                         </a>
                     </li>
@@ -79,19 +79,19 @@
                 <p><strong class="fw-bold">VÍNCULOS DE INTERÉS</strong></p>
                 <ul class="list-unstyled">
                     <li class="mb-2 small">
-                        <a href='/brazaletes' use:link class="link-info text-decoration-none">Brazalete</a>
+                        <a href='/brazaletes' use:link class="link-dark text-decoration-none">Brazalete</a>
                     </li>
                     <li class="mb-2 small">
-                        <a href='/rfid' use:link class="link-info text-decoration-none">Soluciones RFID</a>
+                        <a href='/rfid' use:link class="link-dark text-decoration-none">Soluciones RFID</a>
                     </li>
                     <li class="mb-2 small">
-                        <a href='/ecofriendly' use:link class="link-info text-decoration-none">Ecofriendly</a>
+                        <a href='/ecofriendly' use:link class="link-dark text-decoration-none">Ecofriendly</a>
                     </li>
                     <li class="mb-2 small">
-                        <a href='/hologramas' use:link class="link-info text-decoration-none">Hologramas</a>
+                        <a href='/hologramas' use:link class="link-dark text-decoration-none">Hologramas</a>
                     </li>
                     <li class="mb-2 small">
-                        <a href='/promocionales' use:link class="link-info text-decoration-none">Promocionales</a>
+                        <a href='/promocionales' use:link class="link-dark text-decoration-none">Promocionales</a>
                     </li>
                 </ul>
             </div>
@@ -99,8 +99,12 @@
             <div class="col-12 col-sm-12 col-md-12 col-lg-2 mb-3 d-none d-lg-block">
                 <p><strong class="fw-bold">PAGUE CON</strong></p>
                 <div>
-                    <i class="fab fa-2x fa-cc-visa me-2"></i>
-                    <i class="fab fa-2x fa-cc-mastercard"></i>
+                    <a href="#" class="hover-link">
+                        <i class="fab fa-2x fa-cc-visa me-2"></i>
+                    </a>
+                    <a href="#" class="hover-link">
+                        <i class="fab fa-2x fa-cc-mastercard"></i>
+                    </a>
                 </div>
             </div>
 
@@ -111,49 +115,52 @@
         <div class="container">
 
             <div class="row align-items-center">
-                <div class="col-12 col-md-12 col-lg-6">
-                    <div class="row row-cols-1 g-1">
+                <div class="col-12 col-md-12 col-lg-7">
+                    <div class="row row-cols-1 g-1 mb-2 mb-md-3 mb-lg-1">
                         <div class="col text-center text-lg-start">
-                            <ul class="list-group list-group-horizontal list-group-flush align-items-center">
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small">
-                                    <a href="#" class="link-light text-decoration-none">Inicio</a>
+
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item small">
+                                    <a href="/" class="link-light text-decoration-none" use:link>Inicio</a>
                                 </li>
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small">
-                                    <a href="#" class="link-light text-decoration-none">Nosotros</a>
+                                <li class="list-inline-item small">
+                                    <a href="/nosotros" class="link-light text-decoration-none" use:link>Nosotros</a>
                                 </li>
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small">
-                                    <a href="#" class="link-light text-decoration-none">Tienda</a>
+                                <li class="list-inline-item small">
+                                    <a href="/tienda" class="link-light text-decoration-none" use:link>Tienda</a>
                                 </li>
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small">
-                                    <a href="#" class="link-light text-decoration-none">Blog</a>
+                                <li class="list-inline-item small">
+                                    <a href="/blog" class="link-light text-decoration-none" use:link>Blog</a>
                                 </li>
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small">
-                                    <a href="#" class="link-light text-decoration-none">Contacto</a>
+                                <li class="list-inline-item small">
+                                    <a href="/contacto" class="link-light text-decoration-none" use:link>Contacto</a>
                                 </li>
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small d-none d-md-block d-lg-none">
-                                    <a href="#" class="link-light text-decoration-none">Privacidad</a>
+                                <li class="list-inline-item small d-lg-none">
+                                    <a href="/aviso-de-privacidad" class="link-light text-decoration-none" use:link>Privacidad</a>
                                 </li>
-                                <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white small d-none d-md-block d-lg-none">
-                                    <a href="#" class="link-light text-decoration-none">Términos de uso</a>
+                                <li class="list-inline-item small d-lg-none">
+                                    <a href="/terminos-de-uso-y-condiciones" class="link-light text-decoration-none" use:link>Términos de uso</a>
                                 </li>
                             </ul>
+
                         </div>
                         <div class="col text-center text-lg-start">
-                            <small class="texto-light">Todos los Derechos Reservados {currentYear} © <strong>Brazaletes México S.A. de C.V.</strong></small>
+                            <small class="d-inline-block texto-light">Todos los Derechos Reservados {currentYear} </small>
+                            <small class="d-inline-block">© <strong>Brazaletes México S.A. de C.V.</strong></small>
                         </div>
                     </div>
                 </div>
-                <div class="col d-none d-sm-none d-md-none d-lg-block">
-                    <ul class="list-group list-group-horizontal list-group-flush align-items-center justify-content-end">
-                        <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white">
-                            <a href="#" class="link-light text-decoration-none">Privacidad</a>
+                <div class="col text-center text-lg-end">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item align-middle d-none d-lg-inline">
+                            <a href="/aviso-de-privacidad" class="link-light text-decoration-none" use:link>Privacidad</a>
                         </li>
-                        <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white">
-                            <a href="#" class="link-light text-decoration-none">Términos de uso</a>
+                        <li class="list-inline-item align-middle d-none d-lg-inline">
+                            <a href="/terminos-de-uso-y-condiciones" class="link-light text-decoration-none" use:link>Términos de uso</a>
                         </li>
-                        <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white"><i class="fab fa-2x fa-cc-visa link-light"></i></li>
-                        <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white"><i class="fab fa-2x fa-cc-mastercard link-light"></i></li>
-                        <li class="list-group-item bg-transparent py-0 ps-0 border-0 text-white"><i class="fab fa-2x fa-cc-amex link-light"></i></li>
+                        <li class="list-inline-item align-middle"><i class="fab fa-2x fa-cc-visa link-light"></i></li>
+                        <li class="list-inline-item align-middle"><i class="fab fa-2x fa-cc-mastercard link-light"></i></li>
+                        <li class="list-inline-item align-middle"><i class="fab fa-2x fa-cc-amex link-light"></i></li>
                     </ul>
                 </div>
             </div>
@@ -166,5 +173,17 @@
 <style>
     .texto-light {
         color: rgba(255, 255, 255, 0.75);
+    }
+
+    .link-dark:hover, .link-dark:focus {
+        font-weight: 400 !important;
+    }
+
+    .hover-link {
+        color: #55595c !important;
+    }
+
+    .hover-link:hover, .hover-link:focus {
+        color: #919aa1 !important;
     }
 </style>
