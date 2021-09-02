556da0b9f8ca666c24172f95cc8d2d72747b0bad
Julian Candiani Vélez <jcandianivelez@gmail.com>
2021-09-02 16:46:24 -0500

    Accesos directes a producto en categiorías con 1 elemento.

--- src/components/pages/Brazaletes.svelte
+++ src/components/pages/Brazaletes.svelte
@@ -1,75 +1,82 @@
 <script>
-  import { currentSection, currentIdTipo, apiHost } from "../../stores/stores"
-  
-  import Loading from '../ui/Loading.svelte'
-  import Template from '../ui/Template.svelte'
-  import Jumbotron from '../ui/Jumbotron.svelte'
-  import GridBrazaletes from '../ui/GridBrazaletes.svelte'
-  import GridTiposBrazaletes from "../ui/GridTiposBrazaletes.svelte"
+    import {
+        currentSection,
+        currentIdTipo,
+        apiHost,
+    } from "../../stores/stores";
 
-  export let tipo = null
-  export let location
+    import Loading from "../ui/Loading.svelte";
+    import Template from "../ui/Template.svelte";
+    import Jumbotron from "../ui/Jumbotron.svelte";
+    import GridBrazaletes from "../ui/GridBrazaletes.svelte";
+    import GridTiposBrazaletes from "../ui/GridTiposBrazaletes.svelte";
 
-  $currentSection = 1
+    export let tipo = null;
+    export let location;
 
-  let tipos = []
-  let brazaletes = []
-  let tipoBrazalete = {}
-  let titulo = ''
-  let busqueda = false;
+    $currentSection = 1;
 
-  const updateIdTipo = (event) => {
-    $currentIdTipo = event.detail
-    localStorage.setItem('currentIdTipo', event.detail)
-  }
+    let titulo = "";
+    let promise = null;
+    let tipoBrazalete = {};
 
-  const obtenerBusqueda = async () => {
-    if (tipo !== null) {
-      let response = await fetch(`${$apiHost}/brazaletes/all/${tipo}`)
-      brazaletes = await response.json()
+    const updateIdTipo = (event) => {
+        $currentIdTipo = event.detail;
+        localStorage.setItem("currentIdTipo", event.detail);
+    };
 
-      response = await fetch(`${$apiHost}/tipos/one/${tipo}`)
-      tipoBrazalete = await response.json()
-      titulo = tipoBrazalete.descripcion.toUpperCase()
-    } else {
-      const response = await fetch(`${$apiHost}/tipos/all/${$currentSection}`)
-      tipos = await response.json()
-      titulo = 'Brazaletes México'
+    const obtenerBusqueda = async () => {
+        let response = null;
+        let products = null;
+
+        if (tipo !== null) {
+            products = await fetch(`${$apiHost}/brazaletes/all/${tipo}`);
+
+            response = await fetch(`${$apiHost}/tipos/one/${tipo}`);
+            tipoBrazalete = await response.json();
+
+            titulo = tipoBrazalete.descripcion.toUpperCase();
+        } else {
+            products = await fetch(`${$apiHost}/tipos/all/${$currentSection}`);
+            titulo = "Brazaletes México";
+        }
+
+        return await products.json();
+    };
+
+    $: if (window.location.pathname || tipo) {
+        window.scrollTo(0, 0);
+        promise = obtenerBusqueda();
     }
-  };
-
-  $: if (window.location.pathname || tipo) {
-    window.scrollTo(0, 0);
-    busqueda = obtenerBusqueda();
-  }
 </script>
 
 <svelte:head>
-  <title> Brazaletes México | Brazaletes para todo tipo de eventos </title>
+    <title>Brazaletes México | Brazaletes para todo tipo de eventos</title>
 </svelte:head>
 
 <Template>
+    {#await promise}
+        <Loading />
+    {:then data}
+        <h1 class="text-center my-5">{titulo}</h1>
 
-  {#await busqueda}
-    <Loading />
-  {:then busqueda}
+        {#if Object.keys(tipoBrazalete).length > 0}
+            <div class="container mb-5">
+                <Jumbotron {tipoBrazalete} />
+            </div>
+        {/if}
 
-    <h1 class='text-center my-5'> {titulo} </h1>
-
-    {#if Object.keys(tipoBrazalete).length > 0 }
-      <div class="container mb-5">
-        <Jumbotron {tipoBrazalete} />
-      </div>
-    {/if}
-
-    <div class="container my-5">
-      {#if tipo !== null}
-        <GridBrazaletes {brazaletes} />
-      {:else}
-        <GridTiposBrazaletes on:updateIdTipo={updateIdTipo} {tipos} />
-      {/if}
-    </div>
-
-  {/await}
-
+        <div class="container my-5">
+            {#if tipo !== null}
+                <GridBrazaletes brazaletes={data} />
+            {:else}
+                <GridTiposBrazaletes
+                    on:updateIdTipo={updateIdTipo}
+                    tipos={data}
+                />
+            {/if}
+        </div>
+    {:catch error}
+        <p>{error}</p>
+    {/await}
 </Template>
--- src/components/pages/Promocionales.svelte
+++ src/components/pages/Promocionales.svelte
@@ -1,49 +1,51 @@
 <script>
-  import { currentSection, apiHost } from "../../stores/stores";
+    import { currentSection, apiHost } from "../../stores/stores";
 
-  import GridTiposBrazaletes from "../ui/GridTiposBrazaletes.svelte";
-  import GridBrazaletes from "../ui/GridBrazaletes.svelte";
-  import Template from "../ui/Template.svelte";
-  import Loading from "../ui/Loading.svelte";
+    import GridTiposBrazaletes from "../ui/GridTiposBrazaletes.svelte";
+    import GridBrazaletes from "../ui/GridBrazaletes.svelte";
+    import Template from "../ui/Template.svelte";
+    import Loading from "../ui/Loading.svelte";
 
-  $currentSection = 5;
+    $currentSection = 5;
 
-  let promise;
-  export let tipo = null;
-  export let location = "";
+    let promise;
+    export let tipo = null;
+    export let location = "";
 
-  const obtenerPromcionales = async () => {
-    if (tipo !== null) {
-      const response = await fetch(`${$apiHost}/brazaletes/all/${tipo}`);
-      return await response.json();
-    } else {
-      const response = await fetch(`${$apiHost}/tipos/all/${$currentSection}`);
-      return await response.json();
+    const obtenerPromcionales = async () => {
+        if (tipo !== null) {
+            const response = await fetch(`${$apiHost}/brazaletes/all/${tipo}`);
+            return await response.json();
+        } else {
+            const response = await fetch(
+                `${$apiHost}/tipos/all/${$currentSection}`
+            );
+            return await response.json();
+        }
+    };
+
+    $: if (location.pathname || tipo) {
+        window.scrollTo(0, 0);
+        promise = obtenerPromcionales();
     }
-  };
-
-  $: if (location.pathname || tipo) {
-    window.scrollTo(0, 0);
-    promise = obtenerPromcionales();
-  }
 </script>
 
 <svelte:head>
-  <title>Brazaletes México | Brazaletes Promocionales</title>
+    <title>Brazaletes México | Brazaletes Promocionales</title>
 </svelte:head>
 
 <Template>
-  {#await promise}
-    <Loading />
-  {:then data}
-    <h1 class="text-center my-5">Promocionales</h1>
+    {#await promise}
+        <Loading />
+    {:then data}
+        <h1 class="text-center my-5">Promocionales</h1>
 
-    <div class="container my-5">
-      {#if tipo !== null}
-        <GridBrazaletes brazaletes={data} />
-      {:else}
-        <GridTiposBrazaletes tipos={data} />
-      {/if}
-    </div>
-  {/await}
+        <div class="container my-5">
+            {#if tipo !== null}
+                <GridBrazaletes brazaletes={data} />
+            {:else}
+                <GridTiposBrazaletes tipos={data} />
+            {/if}
+        </div>
+    {/await}
 </Template>
