dd1eebae9c9fd0c840bac8892f2e9dde17678c7c
Julian Candiani Vélez <jcandianivelez@gmail.com>
2021-09-03 15:33:15 -0500

    Se corrigió sección de ocasiones.

--- src/components/ui/OptionMenu.svelte
+++ src/components/ui/OptionMenu.svelte
@@ -1,64 +1,72 @@
 <script>
-  import { currentSection } from "../../stores/stores";
-  import { link } from "svelte-routing";
-  export let seccion = {};
-  let showSubmenu = false;
+    import { currentSection } from "../../stores/stores";
+    import { link } from "svelte-routing";
+    export let seccion = {};
+    let showSubmenu = false;
 
-  const handleMouseOver = (e) => {
-		showSubmenu = true;
-	}
+    console.log(seccion);
 
-  const handleMouseOut = (e) => {
-		showSubmenu = false;
-	}
+    const handleMouseOver = (e) => {
+        showSubmenu = true;
+    };
 
+    const handleMouseOut = (e) => {
+        showSubmenu = false;
+    };
 </script>
 
 {#if seccion.submenus}
-
-  <li class="nav-item me-2 position-relative" on:mouseover={handleMouseOver} on:mouseleave={handleMouseOut}>
-    <a 
-      class="nav-link {seccion.descripcion === "ECOFRIENDLY" ? "link-ecofriendly" : ""} {$currentSection == seccion.id ? 'active' : ''}" 
-      href="/{seccion.slug}"
-      use:link
+    <li
+        class="nav-item me-2 position-relative"
+        on:mouseover={handleMouseOver}
+        on:mouseleave={handleMouseOut}
     >
-      {seccion.descripcion} <i class="fas fa-caret-down ms-2"></i>
-  </a>
+        <a
+            class="nav-link {seccion.descripcion === 'ECOFRIENDLY'
+                ? 'link-ecofriendly'
+                : ''} {$currentSection == seccion.id ? 'active' : ''}"
+            href="/{seccion.slug}"
+            use:link
+        >
+            {seccion.descripcion} <i class="fas fa-caret-down ms-2" />
+        </a>
 
-    {#if showSubmenu}
-      <div class="position-absolute top-100 start-0 w-150 shadow bring-to-front">
-        <ul class="list-unstyled bg-primary">
-          {#each seccion.submenus as {id, descripcion, slug}, i}
-            <li class="small">
-              <a class="nav-link" href="/{slug}" use:link>
-                {descripcion}
-              </a>
-            </li>
-          {/each}
-        </ul>
-      </div>
-    {/if}
-
-  </li>
-
+        {#if showSubmenu}
+            <div
+                class="position-absolute top-100 start-0 w-150 shadow bring-to-front"
+            >
+                <ul class="list-unstyled bg-primary">
+                    {#each seccion.submenus as { id, descripcion, slug }, i}
+                        <li class="small">
+                            <a class="nav-link" href="/{slug}" use:link>
+                                {descripcion}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+        {/if}
+    </li>
 {:else}
-  <li class="nav-item me-2">
-    <a 
-      class="nav-link {seccion.descripcion === "ECOFRIENDLY" ? "link-ecofriendly" : ""} {$currentSection == seccion.id ? 'active' : ''}" 
-      href="/{seccion.slug}"
-      use:link
-    >
-      {seccion.descripcion}
-  </a>
-  </li>
+    <li class="nav-item me-2">
+        <a
+            class="nav-link {seccion.descripcion === 'ECOFRIENDLY'
+                ? 'link-ecofriendly'
+                : ''} {$currentSection == seccion.id ? 'active' : ''}"
+            href="/{seccion.slug}"
+            use:link
+        >
+            {seccion.descripcion}
+        </a>
+    </li>
 {/if}
 
 <style>
-  .bring-to-front {
-    z-index: 1081 !important;
-  }
+    .bring-to-front {
+        z-index: 1081 !important;
+    }
 
-  .w-150 {
-    width: 150% !important;
-  }
+    .w-150 {
+        width: 150% !important;
+    }
 </style>
