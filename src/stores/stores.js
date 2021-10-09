b23a048228939c4c034bd0b7c58ed1112bf81508
Francisco Paz <f.paz85@gmail.com>
2021-10-09 14:07:13 -0500

    Api en el mismo puerto

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store'
 
 export const currentSection = writable(-1)
-export const apiHost = writable('http://162.240.16.72/api/v1')
+export const apiHost = writable('https://162.240.16.72/api/v1')
 // export const apiHost = writable('http://localhost:3001/api/v1')
 export const imagesPath = writable('https://162.240.16.72/docs')
 export const currentIdTipo = writable(localStorage.getItem('currentIdTipo') || -1)
